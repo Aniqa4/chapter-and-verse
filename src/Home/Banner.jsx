6c10f33b52553a31dc36ab1e673379002fb3d7c2
Aniqa <aanika.chowdhury04@gmail.com>
2023-07-28 23:11:41 +0600

    updated hero banner

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -3,11 +3,11 @@ import banner from '../assets/banner.jpg';
 
 function Banner() {
   return (
-    <div className=' container mx-auto relative text-white'>
-        <img src={banner} className='mx-auto lg:h-screen' />
-        <div className=' absolute top-1/3 lg:left-32 lg:right-32 left-5 right-5 bg-sky-500 bg-opacity-40 py-10'>
+    <div className=' mx-auto relative text-white'>
+        <img src={banner} className=' lg:h-screen opacity-70 w-screen' />
+        <div className=' absolute top-1/4 md:top-1/3 lg:left-32 lg:right-32 left-5 right-5 bg-sky-500 bg-opacity-50 p-5 md:py-20'>
           <h1 className=' text-3xl font-semibold text-center'>Chapter&Verse</h1>
-          <p className=' text-center font-semibold text-xl'>Unlock the World of Knowledge - Your One-Stop Bookshop!</p>
+          <p className=' text-center text-gray-200 font-semibold text-xl'>Unlock the World of Knowledge - Your One-Stop Bookshop!</p>
         </div>
     </div>
   )

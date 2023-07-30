53ab1aa91f6cf4db26fb9a964d8df8b2bae2e820
Aniqa <aanika.chowdhury04@gmail.com>
2023-07-31 02:52:22 +0600

    changed banner

--- src/CategoryOfBooks/CategoryOfBooks.jsx
+++ src/CategoryOfBooks/CategoryOfBooks.jsx
@@ -13,6 +13,7 @@ import comics from '../assets/booksByCategory/comics.jpg'
 import childrensBook from '../assets/booksByCategory/childrens-book.jpg'
 import fiction from '../assets/booksByCategory/fiction.jpg'
 import Card from '../Components/Card'
+import { Link } from 'react-router-dom'
 
 function CategoryOfBooks() {
 
@@ -33,21 +34,21 @@ function CategoryOfBooks() {
             <div className=' mb-5 grid grid-cols-4 lg:grid-cols-12 gap-2 p-2'>
                 {
                     description.map((x,index)=>
-                        <button key={index} className=' border'>{x.name}</button>)
+                        <button key={index} className=' border'><Link to={x.route}>{x.name}</Link></button>)
                 }
             </div>
             <div className=' container px-2 md:px-5 lg:px-0 mx-auto grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5'>
                 <Card image={banglaLiterature} categoryName={'Bangla Literature'} route={'/category=bangla-literature'}
                 description={description[11]?.description}></Card>
-                <Card image={religion} categoryName={'Religion'} route={'/category=religion'} description={description[6]?.description}></Card>
+                <Card image={religion} categoryName={'Religion'} route={'/category=religion'} description={description[7]?.description}></Card>
                 <Card image={fiction} categoryName={'Fiction'} route={'/category=fiction'} description={description[0]?.description}></Card>
                 <Card image={SciFi} categoryName={'Science Fiction'} route={'/category=science-fiction'} description={description[2]?.description}></Card>
                 <Card image={nonFiction} categoryName={'Non Fiction'} route={'/category=non-fiction'} description={description[1]?.description}></Card>
                 <Card image={biography} categoryName={'Biography'} route={'/category=biography'} description={description[3]?.description}></Card>
                 <Card image={history} categoryName={'History'} route={'/category=history'} description={description[6]?.description}></Card>
-                <Card image={comics} categoryName={'Comics'} route={'/category=comics'} description={description[7]?.description}></Card>
-                <Card image={science} categoryName={'Science'} route={'/category=science'} description={description[9]?.description}></Card>
-                <Card image={psychology} categoryName={'Psychology'} route={'/category=psychology'} description={description[8]?.description}></Card>
+                <Card image={comics} categoryName={'Comics'} route={'/category=comics'} description={description[8]?.description}></Card>
+                <Card image={science} categoryName={'Science'} route={'/category=science'} description={description[10]?.description}></Card>
+                <Card image={psychology} categoryName={'Psychology'} route={'/category=psychology'} description={description[10]?.description}></Card>
                 <Card image={cookBook} categoryName={'Cook Books'} route={'/category=cook-books'} description={description[5]?.description}></Card>
                 <Card image={childrensBook} categoryName={`Children's Books`} route={`/category=children's-books`} 
                 description={description[4]?.description}></Card>
--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -5,9 +5,9 @@ import banner from '../assets/banner.jpg';
 function Banner() {
   return (
     <div className=' mx-auto relative text-white'>
-        <img src={banner} className=' lg:h-screen opacity-50 w-screen' />
-        <div className=' absolute top-1/4 md:top-1/3 left-0 right-0 bg-slate-700 bg-opacity-40 p-5 md:py-20'>
-          <h1 className=' text-xl md:text-3xl font-semibold text-center'>Chapter&Verse</h1>
+        <img src={banner} className=' opacity-50 w-screen' />
+        <div className=' absolute top-1/3 md:top-1/4 xl:top-1/4 left-0 right-0 bg-slate-700 bg-opacity-40 md:p-5 xl:py-10'>
+          <h1 className=' text-base md:text-3xl font-semibold text-center'>Chapter&Verse</h1>
           <p className=' text-center font-semibold text-xs md:text-xl'>Unlock the World of Knowledge - Your One-Stop Bookshop!</p>
         </div>
     </div>

628e4a3fcfa36931643af4be6dc591c72430aa2a
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-01 03:09:50 +0600

    responsive navigation bar for all devices

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 function Card({ image, categoryName, description, route }) {
   return (
     <div className=' relative text-white'>
-      <img src={image} className=' w-full rounded-xl' />
-      <div className=' absolute top-0 left-0 right-0 bottom-0 p-5 rounded-xl grid items-center bg-slate-900 bg-opacity-40'>
+      <img src={image} className=' w-full' />
+      <div className=' absolute top-0 left-0 right-0 bottom-0 p-5 grid items-center bg-slate-900 bg-opacity-40'>
         <h1 className=' md:text-2xl font-semibold'>{categoryName}</h1>
         <p className='py-2 text-xs md:text-base'>{description}</p>
         <Link to={route}><button className=' bg-slate-800 rounded text-white px-5 py-2 hover:bg-slate-600'>Browse</button></Link>
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,34 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { CgShoppingBag } from 'react-icons/cg';
 import { MdOutlineFavoriteBorder } from 'react-icons/md';
 import { AiOutlineUserAdd } from 'react-icons/ai';
+import { CiMenuKebab } from 'react-icons/ci';
+import { Box, Drawer, Typography } from '@mui/material';
 
 
 function Navbar() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+
+
   return (
     <>
-      <div className=' bg-white fixed top-0 left-0 right-0 z-50 shadow '>
-        <div className='container mx-auto flex justify-between text-slate-700 py-5'>
-          <h1 className=' font-bold uppercase'>Chapter<span className=' text-black'>&</span>Verse</h1>
+      <div className='hidden lg:block bg-white fixed top-0 left-0 right-0 z-50 shadow '>
+        <div className='container mx-auto px-5 flex justify-between text-slate-700 py-5'>
+          <div className='flex items-center gap-20'>
+            <button onClick={() => setIsDrawerOpen(true)} className=' text-xl flex gap-2 items-center'><CiMenuKebab />
+              <span className=' text-base'>Menu</span></button>
+            <h1 className=' font-bold text-sm uppercase'><Link to='/'>Chapter<span className=' text-black'>&</span>Verse</Link></h1>
+          </div>
           <ul className='flex gap-10'>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/all-books'>All Books</Link></li>
             <li><Link to='/category'>Category</Link></li>
-            <li className=' relative text-xl'><Link to='/wishlist'><MdOutlineFavoriteBorder/></Link>
-            <span className=' absolute -top-2 -right-2 text-xs'>10</span></li>
+            <li className=' relative text-xl'><Link to='/wishlist'><MdOutlineFavoriteBorder /></Link>
+              <span className=' absolute -top-2 -right-2 text-xs'>10</span></li>
             <li className=' relative text-xl'><Link to='/cart'><CgShoppingBag /></Link>
-            <span className=' absolute -top-2 -right-2 text-xs'>10</span></li>
+              <span className=' absolute -top-2 -right-2 text-xs'>10</span></li>
           </ul>
-          <button className=' text-2xl'><AiOutlineUserAdd/></button>
+          <button className=' text-2xl'><AiOutlineUserAdd /></button>
         </div>
       </div>
-      <div className=' fixed bottom-10 right-5 md:right-10 rounded-full z-50'>
-        <div className=' relative'>
-          <h1 className=' text-black text-4xl'><CgShoppingBag /></h1>
-          <p className=' absolute top-1/2 -right-1 px-2 py-1 bg-black text-white text-xs rounded-full'>1</p>
+      <div className=' lg:hidden bg-white fixed top-0 left-0 right-0 z-50 shadow '>
+        <div className='container mx-auto px-5 flex justify-between  text-slate-700 py-5'>
+          <button onClick={() => setIsDrawerOpen(true)} className=' text-xl flex gap-2 items-center'><CiMenuKebab />
+            <span className=' text-base'>Menu</span></button>
+          <h1 className=' font-bold text-sm uppercase'><Link to='/'>Chapter<span className=' text-black'>&</span>Verse</Link></h1>
+          <ul className='flex gap-5'>
+            <li className=' relative text-xl'><Link to='/wishlist'><MdOutlineFavoriteBorder /></Link>
+              <span className=' absolute -top-2 -right-2 text-xs'>10</span>
+            </li>
+            <li className=' relative text-xl'><Link to='/cart'><CgShoppingBag /></Link>
+              <span className=' absolute -top-2 -right-2 text-xs'>10</span>
+            </li>
+            <li><button className=' text-xl'><AiOutlineUserAdd /></button></li>
+          </ul>
         </div>
       </div>
+      <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+        <Box p={2} width={'250px'} textAlign={'center'} role='presentation'>
+          <Typography variant='h6' component={'div'}> Side panel</Typography>
+        </Box>
+      </Drawer>
     </>
   )
 }

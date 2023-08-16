54a82e0c56cffd34320701fe402ac4764e8fa95a
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-17 03:53:23 +0600

    implemented filter books by publications

--- src/Publishers/BooksByPublisher.jsx
+++ src/Publishers/BooksByPublisher.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import BookLayout from '../Components/BookLayout';
+import Title from '../Components/Title';
 
 function BooksByPublisher() {
-  return (
-    <div>BooksByPublisher</div>
-  )
+    const data = useParams();
+    const publisher = data?.name;
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://chapter-and-verse-server-side.vercel.app/books-by-publisher/${publisher}`)
+            .then(res => res.json())
+            .then(data => setBooks(data))
+    }, []);
+
+    return (
+        <div className='container mx-auto pt-1'>
+            <Title title={`Books of ${publisher}`} />
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5 justify-center mx-5'>
+                {
+                    books?.map(x => <BookLayout key={x._id} bookImage={x.bookImage} bookName={x.bookName} price={x.price} route={`/categories/${x.category}/${x.bookName}`} />)
+                }
+            </div>
+        </div>
+    )
 }
 
 export default BooksByPublisher

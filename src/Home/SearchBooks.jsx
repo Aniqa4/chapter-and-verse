185cf72feb191233372a1a001d5582bd8b1bb5fe
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-25 18:20:50 +0600

    search bar on home page

--- src/Home/SearchBooks.jsx
+++ src/Home/SearchBooks.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BiSearchAlt2 } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 function SearchBooks() {
     const [searchedItem, setSearchedItem] = useState('')
@@ -32,22 +33,23 @@ function SearchBooks() {
         <div className=' container mx-auto'>
             <form onSubmit={handleSearch} className='flex justify-center mt-10'>
                 <div className=' relative  w-1/2'>
-                    <input type="search" name="search" className=' absolute top-0 left-0 right-0 bottom-0' />
+                    <input type="search" name="search" className=' absolute top-0 left-5 right-0 bottom-0' />
                 </div>
                 <button className=' text-2xl flex items-center bg-gray-800 text-white p-2'><BiSearchAlt2 /></button>
             </form>
-            <div className=' relative'>
+            <div className=' relative' >
                 {showResults && (
                     <div className="absolute top-0 left-0 right-0 bg-opacity-75 flex items-center justify-center z-50">
-                        <div className="bg-gray-100 p-4 w-1/2">
+                        <div className="bg-gray-50 p-4 w-1/2  overflow-y-auto h-60">
                             {searchResult.length === 0 ? (
                                 <p className="text-gray-800">No results found.</p>
                             ) : (
                                 <ul>
                                     {searchResult.map((book, index) => (
                                         <li key={index} className="mb-2">
-                                            <h3 className=" text-blue-600">{book.bookName}</h3>
-                                            {/* Add other information about the book, like author, description, etc. */}
+                                            <h3 className=" text-gray-600 hover:text-blue-600" title='Click here to visit'>
+                                                <Link to={`/categories/${book?.category}/${book?.bookName}`}>{book.bookName}</Link>
+                                            </h3>
                                         </li>
                                     ))}
                                 </ul>

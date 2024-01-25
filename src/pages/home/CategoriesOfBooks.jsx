b0985277a65754bf2042fced156a39c6df93ab3b
Aniqa <aanika.chowdhury04@gmail.com>
2024-01-25 22:05:52 +0600

    modified design

--- src/pages/home/CategoriesOfBooks.jsx
+++ src/pages/home/CategoriesOfBooks.jsx
@@ -4,7 +4,7 @@ import Card from '../../components/Card';
 import BookCategories from '../../Hooks/BookCategories';
 
 function CategoriesOfBooks() {
-  const categories=BookCategories();
+  const categories = BookCategories();
 
   return (
     <div>
@@ -12,7 +12,11 @@ function CategoriesOfBooks() {
       <div className='container px-2 md:px-5 lg:px-0 mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 md:gap-5'>
         {
           categories?.map((x, index) =>
-            <Card key={index} image={x.image} categoryName={x.name} route={`categories/${x.name}`}></Card>)
+            <Card key={index}
+              image={x.image}
+              categoryName={x.name}
+              route={`categories/${x.name}`}>
+            </Card>)
         }
 
       </div>
--- src/pages/home/NewArrivals.jsx
+++ src/pages/home/NewArrivals.jsx
@@ -18,10 +18,15 @@ function NewArrivals() {
         <Title title={'New arrivals'} />
         <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5 justify-center mx-5 md:mx-0'>
           {
-            books?.map(x => <BookLayout key={x?._id} product_id={x._id} bookImage={x.bookImage} bookName={x.bookName} price={x.price} route={`/categories/${x?.category}/${x.bookName}`} />)
+            books?.map(x => <BookLayout key={x?._id}
+              product_id={x._id} bookImage={x.bookImage}
+              bookName={x.bookName} price={x.price}
+              route={`/categories/${x?.category}/${x.bookName}`} />)
           }
         </div>
-        <p className=' text-center mt-5 underline text-blue-400'><Link to="books">See More</Link></p>
+        <p className=' text-center mt-5 underline text-blue-400'>
+          <Link to="books">See More</Link>
+        </p>
       </div>
     </div>
   )
